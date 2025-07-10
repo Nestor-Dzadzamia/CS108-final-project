658945cfeb221feeaa07fd2ddfa564c9851736ae
S-MAMAMTAVRISHVILI <smama23@freeuni.edu.ge>
2025-07-10 19:36:28 +0400

    Taking quizzes fully implemented, excluding very few extensions

--- demo/src/main/java/Models/PossibleAnswer.java
+++ demo/src/main/java/Models/PossibleAnswer.java
@@ -5,6 +5,23 @@ public class PossibleAnswer {
     private long questionId;
     private String possibleAnswerText;
 
+    public  PossibleAnswer() {
+
+    }
+
+
+    public PossibleAnswer(long l, String s) {
+        this.questionId = l;
+        this.possibleAnswerText = s;
+    }
+
+    public PossibleAnswer(long l, long l1, String s) {
+        this.possibleAnswerId = l;
+        this.questionId = l1;
+        this.possibleAnswerText = s;
+    }
+
+
     // Getters and Setters
     public long getPossibleAnswerId() { return possibleAnswerId; }
     public void setPossibleAnswerId(long possibleAnswerId) { this.possibleAnswerId = possibleAnswerId; }
