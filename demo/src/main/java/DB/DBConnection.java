68ad47a6bdd2bc4f9e746d22d637075e223868fc
Nestor-Dzadzamia <ndzad22@freeuni.edu.ge>
2025-07-30 20:40:52 +0400

    Update DBConnection.java

--- demo/src/main/java/DB/DBConnection.java
+++ demo/src/main/java/DB/DBConnection.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/quiz_app";
-    private static final String USER = "root";
-    private static final String PASSWORD = ""; //your password
+    private static final String USER = "";
+    private static final String PASSWORD = ""; 
 
     public static Connection getConnection() throws SQLException {
         try {
